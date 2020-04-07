aacfe494e28ef0b2be0163cf71c8ffe2b1bad249
MarkTijssen <47105275+MarkTijssen@users.noreply.github.com>
2020-04-07 11:12:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import base64
+from google.cloud import bigquery
+
+project_id = '901492054369'
+datasetId = "24617418"
+client = bigquery.Client(project_id)
+
+def changeLabelsOnSingleTable(datasetId, tableId):
+  dataset = client.get_dataset(datasetId)
+  table_ref = dataset.table(tableId)
+  table = client.get_table(table_ref)
+  print(f"{datasetId}.{tableId} wordt aangepast.")
+  table.labels = dataset.labels
+  table = client.update_table(table, ["labels"])
+  print(f"\t{table.table_id} is updatet!")
+
+def updateAllNoneLabeledTables(datasetId):
+  # datasetId = "24617418"
+  dataset = client.get_dataset(datasetId)
+  tables = list(client.list_tables(dataset))
+  for table in tables:
+    table_ref = dataset.table(table.table_id)
+    table = client.get_table(table_ref)
+    t_labels = table.labels
+    if t_labels == {}:
+      print(f"Adding labels to {table.table_id}")
+      changeLabelsOnSingleTable(dataset.dataset_id, table.table_id)
+
+
+def run(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+
+    updateAllNoneLabeledTables(datasetId)
+
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+
