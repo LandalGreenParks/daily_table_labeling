85272419e301ff4f98b9dfae7ff18ee25baa1684
Mark Tijssen <marktijssen@gmail.com>
2020-04-07 15:53:10 +0200

    outputJson added raw event message added

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ def run(event, context):
 
     # changeLabelsOnSingleTable(datasetId)
 
+    # pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-
 
 
     # message = pubsub_message["resource"]["labels"]["dataset_id"]
     message = pubsub_message
-    # message2 = dict(pubsub_message)["resource"]
+    message2 = event
 
     print("Starting the function!")
     import time
@@ -56,6 +56,6 @@ def run(event, context):
 
     print("update the sheet")
     worksheet.update_acell("C5", f"Message: {message}")
-    # worksheet.update_acell("C6", f"Message2: {message2}")
+    worksheet.update_acell("C6", f"Message2: {message2}")
     # print(google.cloud.functions.Context)
 
